9c466a365410f0436d94f808ba05a07c85539e19
lafatha <athallahfatha@gmail.com>
2026-05-26 23:32:08 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Education from "./components/Education";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import GitHubActivity from "./components/GitHubActivity";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 export default function Home() {
@@ -17,7 +16,6 @@ export default function Home() {
         <Skills />
         <Projects />
         <GitHubActivity />
-        <Contact />
         <Footer />
         </div>
       </main>
